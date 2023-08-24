b066d6dd24160cb80a5d4ba0fcbafc0a3b12e3a8
hesam yavari <hyavari1@gmail.com>
2023-08-24 17:38:22 +0200

    add helm package

--- kubespraysetup.sh
+++ kubespraysetup.sh
@@ -2,17 +2,18 @@
 sudo apt update && sudo apt install git python3 python3-pip -y
 git clone https://github.com/kubernetes-incubator/kubespray.git
 cd kubespray
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
 
 cp -rfp inventory/sample inventory/mycluster
-declare -a IPS=(ip)
-echo ${IPS[@]} node1 >> /etc/hosts 
+declare -a IPS=(192.168.163.129)
+sudo echo " ${IPS[@]} node1" >> /etc/hosts 
 CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
-
+ssh-keygen
 ssh-copy-id $USER@${IPS[@]}
 #add ansible user to sudo group
-echo "@USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/@USER
+echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USER
 #disable firewall on all the nodes
+pwd
 ansible all -i inventory/mycluster/hosts.yaml -m shell -a "sudo systemctl stop firewalld && sudo systemctl disable firewalld"
 #enable IPv4 forwarding and disable swap on all the nodes
 ansible all -i inventory/mycluster/hosts.yaml -m shell -a "echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf"
